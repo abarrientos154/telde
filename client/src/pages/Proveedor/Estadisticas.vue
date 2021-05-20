f23eba77a4438f2186ca8a6238514fe20cae67b1
reile <reilefer17@gmail.com>
2021-05-20 11:17:57 -0400

    detalles

--- client/src/pages/Proveedor/Estadisticas.vue
+++ client/src/pages/Proveedor/Estadisticas.vue
@@ -18,22 +18,34 @@
             <div class="text-caption text-grey-5">Dinero de tu monedero</div>
             <div class="row items-center justify-between">
               <div class="text-h4 text-bold text-white q-mr-sm">€35,291</div>
-              <q-btn class="q-pa-xs col" color="light-green-13" text-color="white" label="Solicitar" style="border-radius: 15px;" no-caps/>
+              <q-btn class="q-pa-xs col" color="light-green-13" text-color="white" label="Solicitar" style="border-radius: 15px; max-width: 200px" no-caps/>
             </div>
           </q-card-section>
         </q-card>
       </div>
       <div class="q-pa-lg text-center text-h6 text-grey-8">Estadísticas de ventas</div>
-      <div class="row q-px-lg q-mb-lg">
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-mb-md">
+      <div class="row justify-center q-px-lg q-mb-lg">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-mb-md q-px-sm">
           <q-select v-model="report" :options="typeReport" label="Seleccione el tipo de reporte" />
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-mb-md">
-          <q-select v-model="fecha" :options="report == 'Reporte mensual' ? meces : años" label="Seleccione el mes">
+        <div v-if="report == 'Reporte mensual' || report == 'Reporte anual'" class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-mb-md q-px-xs">
+          <q-select v-model="fecha" :options="report == 'Reporte mensual' ? meces : años" :label="report == 'Reporte mensual' ? 'Seleccione el mes' : report == 'Reporte anual' ? 'Seleccione el año' : ''">
             <template v-slot:append>
               <q-icon name="event" />
             </template>
           </q-select>
+        </div>
+        <div v-else-if="report == 'Reporte semanal'" class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-mb-md q-px-xs">
+          <q-input @click="fch = !fch" v-model="fecha" label="Ingrese la semana">
+            <template v-slot:append>
+              <q-icon name="event" />
+            </template>
+          </q-input>
+          <q-dialog v-model="fch">
+            <div>
+              <q-date v-model="fechaReport" @input="selecFecha()" range />
+            </div>
+          </q-dialog>
         </div>
       </div>
       <div class="row justify-center q-mb-lg">
@@ -50,12 +62,12 @@
       </div>
       <div class="text-center text-h6 text-grey-8 q-mb-sm">Ultimos productos vendidos</div>
       <div class="row justify-center full-width q-mb-xl">
-        <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 q-mb-md q-px-sm" v-for="index in 4" :key="index">
+        <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 q-mb-md q-px-sm" v-for="index in prod" :key="index">
           <q-img src="nopublicidad.jpg" style="height: 175px; width: 100%;" basic/>
           <div>
             <div class="row items-center justify-between">
               <q-rating max="5" size="16px" v-model="rating" color="yellow-8" icon="star_border" icon-selected="star" icon-half="star_half" no-dimming />
-              <div style="font-size: 10px">Fecha de venta</div>
+              <div style="font-size: 8px">Fecha de venta</div>
             </div>
             <div class="text-caption">Producto</div>
             <div class="row items-center justify-between">
@@ -66,7 +78,7 @@
         </div>
       </div>
       <div class="row justify-center q-mb-md">
-        <q-btn rounded class="q-pa-xs" color="light-green-13" text-color="white" label="Ver más productos" style="width: 90%;" no-caps/>
+        <q-btn rounded class="q-pa-xs" color="light-green-13" text-color="white" @click="ver()" label="Ver más productos" style="width: 90%;" no-caps/>
       </div>
     </div>
   </div>
@@ -82,30 +94,45 @@ export default {
     return {
       chartData: [
         ['Genre', 'Ganancia', 'Presupuesto', { role: 'annotation' }],
-        ['01', 10, 24, ''],
-        ['02', 16, 22, ''],
-        ['03', 28, 19, ''],
-        ['04', 28, 19, ''],
-        ['05', 28, 19, '']
+        ['01', 20, 5, ''],
+        ['02', 25, 8, ''],
+        ['03', 15, 5, ''],
+        ['04', 35, 4, ''],
+        ['05', 10, 35, '']
       ],
       chartOptions: {
         height: 200,
         legend: { position: 'none' },
-        bar: { groupWidth: '10%' },
+        bar: { groupWidth: '20%' },
         isStacked: true,
         colors: ['yellow', 'blue']
       },
-      typeReport: ['Reporte mensual'],
+      typeReport: ['Reporte semanal', 'Reporte mensual', 'Reporte anual'],
       meces: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-      años: [],
+      años: ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'],
+      fechaReport: { from: '2020/07/08', to: '2020/07/17' },
+      fch: false,
       report: '',
       fecha: '',
+      prod: 4,
+      verProd: false,
       rating: 4
     }
   },
   mounted () {
   },
   methods: {
+    selecFecha () {
+      this.fecha = this.fechaReport.from + ' - ' + this.fechaReport.to
+    },
+    ver () {
+      this.verProd = !this.verProd
+      if (this.verProd) {
+        this.prod = 8
+      } else {
+        this.prod = 4
+      }
+    }
   }
 }
 </script>
