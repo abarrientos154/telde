4e844f4f41f0713f86e43c34b25b6ce2147ba537
example <example@example.com>
2021-06-03 22:43:07 -0400

    fix

--- client/src/pages/Registro/PagoMembresia.vue
+++ client/src/pages/Registro/PagoMembresia.vue
@@ -137,7 +137,6 @@ export default {
       slide: 1,
       id: this.$route.params,
       cantidad: 1,
-      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
       apiUrl: ''
     }
   },
